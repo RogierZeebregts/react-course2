77f94b98fae784aac30a2f6ea78173715705c64d
Rogier Zeebregts <rogier.zeebregts@freshheads.com>
2020-05-04 16:47:56 +0200

    Div

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './modal.module.css'
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => (
-    <>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-        <Backdrop
-            show={props.show}
-            clicked={props.modalClosed}
-        />
-    </>
-)
+class Modal extends Component {
+    shouldComponentUpdate(nextProps) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+            </>
+        )
+    }
+}
 
 export default Modal
